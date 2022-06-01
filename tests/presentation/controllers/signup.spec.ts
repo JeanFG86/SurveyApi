33541abd0ba167782ec55b320448dd3fac7f1621
jeanfg86 <jeanfg86@gmail.com>
2022-05-31 22:10:15 -0300

    move code to own file

--- tests/presentation/controllers/signup.spec.ts
+++ tests/presentation/controllers/signup.spec.ts
@@ -1,19 +1,4 @@
-class SignUpController {
-  handle (httpRequest: any): any {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
-      }
-    }
-  }
-}
+import { SignUpController } from '@/presentation/controllers'
 
 describe('SignUp Controller', () => {
   it('should return 400 if no name is provided', () => {
