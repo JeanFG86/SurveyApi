6fc4483c9d6b76146e3e4787918d6e37fcb2d2fb
jeanfg86 <jeanfg86@gmail.com>
2022-06-21 21:10:13 -0300

    remove code duplication

--- tests/presentation/controllers/login/login.spec.ts
+++ tests/presentation/controllers/login/login.spec.ts
@@ -1,15 +1,22 @@
 import { LoginController } from '@/presentation/controllers/login'
 import { InvalidParamError, MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers'
-import { EmailValidator } from '@/presentation/protocols'
+import { EmailValidator, HttpRequest } from '@/presentation/protocols'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('Login Controller', () => {
   let sut: LoginController
   let emailValidator: MockProxy<EmailValidator>
+  let fakeRequest: MockProxy<HttpRequest>
   beforeAll(() => {
     emailValidator = mock()
     emailValidator.isValid.mockReturnValue(true)
+    fakeRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
   })
 
   beforeEach(() => {
@@ -43,28 +50,15 @@ describe('Login Controller', () => {
   it('Should return 400 if an invalid email is provided', async () => {
     emailValidator.isValid.mockReturnValueOnce(false)
 
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    }
-
-    const httpResponse = await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(fakeRequest)
 
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 
   it('Should call EmailValidator with correct email', async () => {
     const isValidSpy = jest.spyOn(emailValidator, 'isValid')
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    }
 
-    await sut.handle(httpRequest)
+    await sut.handle(fakeRequest)
 
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
