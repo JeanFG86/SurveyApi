7ab4d1042d0330b0b891ee0538fb2dbc4e96d136
jeanfg86 <jeanfg86@gmail.com>
2022-06-21 21:03:15 -0300

    ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,10 @@
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers'
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class LoginController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body?.email) {
       return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
@@ -10,6 +12,7 @@ export class LoginController implements Controller {
     if (!httpRequest.body?.password) {
       return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
     }
+    this.emailValidator.isValid(httpRequest.body?.email)
     return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
   }
 }
--- tests/presentation/controllers/login/login.spec.ts
+++ tests/presentation/controllers/login/login.spec.ts
@@ -1,15 +1,19 @@
 import { LoginController } from '@/presentation/controllers/login'
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers'
+import { EmailValidator } from '@/presentation/protocols'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('Login Controller', () => {
   let sut: LoginController
+  let emailValidator: MockProxy<EmailValidator>
   beforeAll(() => {
-
+    emailValidator = mock()
+    emailValidator.isValid.mockReturnValue(true)
   })
 
   beforeEach(() => {
-    sut = new LoginController()
+    sut = new LoginController(emailValidator)
   })
 
   it('Should return 400 if no email is provided', async () => {
@@ -18,17 +22,35 @@ describe('Login Controller', () => {
         password: 'any_password'
       }
     }
+
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   it('Should return 400 if no password is provided', async () => {
     const httpRequest = {
       body: {
-        email: 'any_email@,ail.com'
+        email: 'any_email@mail.com'
       }
     }
+
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  it('Should call EmailValidator with correct email', async () => {
+    const isValidSpy = jest.spyOn(emailValidator, 'isValid')
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
