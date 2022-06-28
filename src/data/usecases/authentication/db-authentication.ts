7bde4d52e611e3a13adfb02c52ef49bc378e7801
jeanfg86 <jeanfg86@gmail.com>
2022-06-28 18:47:54 -0300

    Ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,10 @@
+import { LoadAccountByEmailRepository } from '@/data/protocols'
+import { Authentication } from '@/domain/usecases'
+
+export class DbAuthentication implements Authentication {
+  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) { }
+  async auth (input: Authentication.Input): Promise<Authentication.OutPut> {
+    await this.loadAccountByEmailRepository.load(input.email)
+    return await new Promise(resolve => resolve({ token: undefined }))
+  }
+}
---
+++ tests/data/usecases/authentication/db-authentication.spec.ts
@@ -0,0 +1,30 @@
+import { LoadAccountByEmailRepository } from '@/data/protocols'
+import { DbAuthentication } from '@/data/usecases/authentication'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('DbAuthentication UseCase', () => {
+  let sut: DbAuthentication
+  let fakeLoadAccount: MockProxy<LoadAccountByEmailRepository>
+  beforeAll(() => {
+    fakeLoadAccount = mock()
+    fakeLoadAccount.load.mockResolvedValue({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+  })
+
+  beforeEach(() => {
+    sut = new DbAuthentication(fakeLoadAccount)
+  })
+
+  it('Should call LoadAccountByEmailRepository with correct email', async () => {
+    await sut.auth({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(fakeLoadAccount.load).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
