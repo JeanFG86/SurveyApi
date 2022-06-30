0e45afef888253977ae5d925499338967e004f40
jeanfg86 <jeanfg86@gmail.com>
2022-06-29 21:50:54 -0300

    Ensure JwtAdapter throws if sign throw

--- tests/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -16,13 +16,25 @@ describe('JWT Adapter', () => {
 
   it('Should call sign with correct values', async () => {
     const signSpy = jest.spyOn(fakeJwt, 'sign')
+
     await sut.encrypt({ value: 'any_id' })
+
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
   it('Should return a token on sign success', async () => {
     fakeJwt.sign.mockImplementationOnce(() => 'any_token')
+
     const accessToken = await sut.encrypt({ value: 'any_id' })
+
     expect(accessToken).toEqual({ token: 'any_token' })
+  })
+
+  it('Should throw if sign throws', async () => {
+    fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error') })
+
+    const promise = sut.encrypt({ value: 'any_id' })
+
+    await expect(promise).rejects.toThrow(new Error('token_error'))
   })
 })
