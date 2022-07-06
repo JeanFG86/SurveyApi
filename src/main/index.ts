d888f812e7911158454fc147b86a2eb9cb2cf598
jeanfg86 <jeanfg86@gmail.com>
2022-07-06 20:55:10 -0300

    configure nodemon and tsnd

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import './config/module-alias'
+import env from './config/env'
 import app from './config/app'
 import { MongoHelper } from '@/infra/db/mongodb/helpers'
-import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl).then(() => {
   app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
