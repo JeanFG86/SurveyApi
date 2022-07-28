2b210755ea71fd8dfd8d5fb0b07ec9b91edcf671
jeanfg86 <jeanfg86@gmail.com>
2022-07-28 18:42:40 -0300

    ensure survey result route returns 200 on save survey result with accessToken

--- tests/main/routes/survey-result-routes.test.ts
+++ tests/main/routes/survey-result-routes.test.ts
@@ -2,17 +2,17 @@ import request from 'supertest'
 import app from '@/main/config/app'
 import { MongoHelper } from '@/infra/db/mongodb/helpers'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '@/main/config/env'
 
 let surveyCollection: Collection
 let accountCollection: Collection
 
-/*
 const makeAccessToken = async (): Promise<string> => {
   const res = await accountCollection.insertOne({
     name: 'Jean',
     email: 'jeanfg86@gmail.com',
-    password: '123',
-    role: 'admin'
+    password: '123'
   })
 
   const id = res.insertedId
@@ -25,7 +25,6 @@ const makeAccessToken = async (): Promise<string> => {
 
   return _token
 }
-*/
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -51,6 +50,30 @@ describe('Survey Routes', () => {
           answer: 'any_answer'
         })
         .expect(403)
+    })
+
+    it('Should return 200 on save survey result with accessToken', async () => {
+      const _token = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }
+        ],
+        date: new Date()
+      })
+      await request(app)
+        .put(`/api/surveys/${res.insertedId.toString()}/results`)
+        .set('x-access-token', _token)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
