381ffbd8d8fda81fb26f05418e4847a258466353
jeanfg86 <jeanfg86@gmail.com>
2022-08-07 12:04:35 -0300

    ensure LoadSurveyResultController returns 500 if LoadSurveyResult throws

--- tests/presentation/controllers/survey-result/load-survey-result/load-survey-resutt-controller.spec.ts
+++ tests/presentation/controllers/survey-result/load-survey-result/load-survey-resutt-controller.spec.ts
@@ -97,4 +97,12 @@ describe('LoadSurveyResult Controller', () => {
 
     expect(loadSpy).toHaveBeenCalledWith('any_id')
   })
+
+  it('Should return 500 if LoadSurveyResult throws', async () => {
+    fakeLoadSurveyResult.load.mockRejectedValueOnce(new Error())
+
+    const httpResponse = await sut.handle(fakeRequest)
+
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
