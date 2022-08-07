d81294922038e70f70ce5a0099041342a871b532
jeanfg86 <jeanfg86@gmail.com>
2022-08-07 12:25:57 -0300

    ensure load survey route returns 200 on success

--- tests/main/routes/survey-result-routes.test.ts
+++ tests/main/routes/survey-result-routes.test.ts
@@ -83,5 +83,26 @@ describe('Survey Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    it('Should return 200 on load survey result with accessToken', async () => {
+      const _token = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }
+        ],
+        date: new Date()
+      })
+      await request(app)
+        .get(`/api/surveys/${res.insertedId.toString()}/results`)
+        .set('x-access-token', _token)
+        .expect(200)
+    })
   })
 })
