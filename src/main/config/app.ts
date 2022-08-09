367f769496eca899a21cc3eb2566ed0b5c5eac96
jeanfg86 <jeanfg86@gmail.com>
2022-08-09 16:37:06 -0300

    add static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddleware from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
+import setupStaticFiles from './static-files'
 
 const app = express()
+setupStaticFiles(app)
 setupMiddleware(app)
 setupRoutes(app)
 export default app
